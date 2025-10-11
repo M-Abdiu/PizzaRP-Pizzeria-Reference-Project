df89c7da12b06296e10e7ce79f92503178b8cfec
M-Abdiu <mehmedali.abdiu@students.fhnw.ch>
2025-10-11 09:45:03 +0200

    Update beispiele_schleifen.py

--- .devcontainer/Block 3/beispiele_schleifen.py
+++ .devcontainer/Block 3/beispiele_schleifen.py
@@ -12,3 +12,23 @@ for i in range(1, 11, 1):   #zeigt in der Klammer an von wo bis wo gezählt wird
         print(i)             #Alles was eingerückt ist wird für jeden Wert von i ausgeführt
 
 
+# break und continue        # Man möchte die Schleife beenden 
+num = 0
+
+while num < 10:
+    num += 1
+    
+    if num == 5:
+        print("Skip!")
+        continue                       #weiter mit dem nächsten inkrement. Heisst 5 wird hier übersprungen und es geht mit 6 weiter
+    if num == 8:
+        print("Breaking at 8!")
+        break                           #Wenn ich bei 8 bin, möchte ich raus aus der Schleife. man geht zur nächsten Zeile ausserhalb des Kasten
+
+    print("Derzeitige Zahl: ", num)
+
+print("Schleife beendet...")
+
+#mit else
+else:
+    print("Nummer nicht gefunden")  # Wenn die break funktion nicht durchgeführt wird. ist auf der Höhe des while loops
