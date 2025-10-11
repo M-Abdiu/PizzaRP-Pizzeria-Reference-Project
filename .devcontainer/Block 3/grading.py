217bf1fab2da4eb923e3454f49d37859b97457ad
M-Abdiu <mehmedali.abdiu@students.fhnw.ch>
2025-10-11 09:45:05 +0200

    Create grading.py

---
+++ .devcontainer/Block 3/grading.py
@@ -0,0 +1,6 @@
+name = input ("Bitte Namen eingeben: ")
+while (name.strip().lower() != "stop")      # strip nimmt die leerzeichen weg. #lower macht kleinbuchstaben raus
+    if not name:                            #leer
+        print("Sie haben keinen Namen eingegeben")
+    else: #weiter machen mit Noten eingabe usw. 
+        
