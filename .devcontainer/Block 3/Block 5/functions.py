2162d7bdcc3b809ef9a1270cd3611aee4f48d91b
M-Abdiu <mehmedali.abdiu@students.fhnw.ch>
2025-10-18 09:49:59 +0200

    Update functions.py

---
+++ .devcontainer/Block 3/Block 5/functions.py
@@ -0,0 +1,12 @@
+def main():
+    wert = 10
+    print("In EUR sind das")
+    umrechnung(wert)
+    
+
+def umrechnung(wert):
+    in_eur = wert * 0.9
+    print("EUR", in_eur)
+          
+
+main()
