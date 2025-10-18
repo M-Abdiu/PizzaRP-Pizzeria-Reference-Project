2f9837b56d0969814d0a62439e07f99bcbd8d758
M-Abdiu <mehmedali.abdiu@students.fhnw.ch>
2025-10-18 10:34:27 +0200

    Create exercise_Translator.py

---
+++ .devcontainer/Block 3/Block 5/exercise_Translator.py
@@ -0,0 +1,14 @@
+from deep_translator import GoogleTranslator    # nach der installation von pypi trotzdem noch importieren 
+
+#Aufruf (Stop wort ist: "stop")
+text = str(input("Bitte Text eingeben:"))
+
+#Aufruf deep_translator (von Google Translate)
+def translation(text)->str:             #->str dient nur um zu wissen was wiedergegeben wird.
+    translated = GoogleTranslator(source='auto', target='en').translate(text=text)
+    return translated
+
+while text.strip().lower() != "stop":              #.strip() = blanks löschen .lower() = alles in Kleinbuchstaben machen
+    print(translation(text))
+    text = str(input("Bitte Text eingeben:"))     # bis der User stop eingibt, ist die Schleife "Endlos"
+print("Programmende...Bye")                        # geht nich zu diesem print ohne das die schleife beendet ist. 
